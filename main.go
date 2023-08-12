c2f35e7899eee5a593325330801563d39c382b76
ruberson1 <ruberson.as@gmail.com>
2023-08-12 13:44:50 -0300

    feat: add router subpackage

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import ("github.com/gin-gonic/gin")
+import "github.com/Ruberson1/api-gojobs/router"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	router.Initialize()
 }
