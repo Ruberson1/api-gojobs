b36dcd1b2c862727348f7e79ac35feb67291bd4e
ruberson1 <ruberson.as@gmail.com>
2023-08-13 09:06:07 -0300

    feat: config and schemas

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/Ruberson1/api-gojobs/router"
+import (
+	"github.com/Ruberson1/api-gojobs/config"
+	"github.com/Ruberson1/api-gojobs/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
