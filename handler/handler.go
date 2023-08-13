0a7351df586f6937535237813487eed5ae2400e7
ruberson1 <ruberson.as@gmail.com>
2023-08-12 21:52:17 -0300

    feat: add handlers route

--- handler/handler.go
+++ handler/handler.go
@@ -1,37 +1,3 @@
 package handler
 
-import (
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
-
-func CreatOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
-}
-
-func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
-}
-
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "UPDATE Opening",
-	})
-}
-
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
-}
-
-func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
-}
