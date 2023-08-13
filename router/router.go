841c819426513d44947aeb1087a558235ddd5440
ruberson1 <ruberson.as@gmail.com>
2023-08-12 21:32:54 -0300

    feat: create routes

--- router/router.go
+++ router/router.go
@@ -1,13 +1,16 @@
-package router 
+package router
 
-import ("github.com/gin-gonic/gin")
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Initialize() {
+	// initialize router
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	// initializeRoutes
+	initializeRoutes(r)
+
+	// Run the server
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
